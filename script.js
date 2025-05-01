9db6068b862840be15d66237457b9fbbbdc9bd97
Bibechana9841 <bk17071321@student.ku.edu.np>
2025-05-01 20:20:54 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Basic script to handle the contact form submission
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for your message!");
+  });
+  
